12e082cefb2f38409b35ad7078cc63f229491174
SatsuyaSystems <projectgremory.api@yahoo.com>
2025-08-19 19:17:56 +0200

    modal addon

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import yaml, logging, os, time, discord, pytz, datetime
 from colorama import Fore, init
 from discord import app_commands
 
+from lib.load_modals import load_modals
 from lib.load_tasks import load_tasks
 from lib.global_registry import g_data
 from lib.load_commands import load_commands
@@ -17,40 +18,37 @@ cfg = g_data.get_or_create(
 
 init(convert=True, autoreset=True)
 
-intents     : discord.Intents           = discord.Intents().all()
-client      : discord.Client            = discord.Client(intents=intents,member_cache_flags=discord.MemberCacheFlags.all())
-tree        : app_commands.CommandTree  = app_commands.CommandTree(client)
-client.tree                             = tree
+intents: discord.Intents = discord.Intents().all()
+client: discord.Client = discord.Client(intents=intents, member_cache_flags=discord.MemberCacheFlags.all())
+tree: app_commands.CommandTree = app_commands.CommandTree(client)
+client.tree = tree
 
 if not os.path.exists('./logs'):
     os.mkdir('./logs')
 
 logging.basicConfig(
-    level= int( cfg.data['bot']['log_level'] ),
-    format= (f'[%(asctime)s] {Fore.YELLOW} {"[%(levelname)s]":7} {Fore.RESET} %(message)s'),
+    level=int(cfg.data['bot']['log_level']),
+    format=(f'[%(asctime)s] {Fore.YELLOW} {"[%(levelname)s]":7} {Fore.RESET} %(message)s'),
     handlers=[
-        logging.FileHandler(f"./logs/{  time.strftime('%Y-%m-%d_%H_%M_%S') }.log",'w','utf-8'),
+        logging.FileHandler(f"./logs/{time.strftime('%Y-%m-%d_%H_%M_%S')}.log", 'w', 'utf-8'),
         logging.StreamHandler()
     ]
 )
+
 
 @client.event
 async def on_ready():
     await load_tasks(client, cfg)
     await load_commands(client, cfg)
     await load_events(client, cfg)
+    await load_modals(client, cfg)
 
     if cfg.data['dc']['sync_guild'] == -1:
-        await client.tree.sync() 
+        await client.tree.sync()
     else:
         await client.tree.sync(guild=discord.Object(id=cfg.data['dc']['sync_guild']))
-    
+
     logging.info(f"Bot is ready as {client.user.name}#{client.user.discriminator} ({client.user.id})")
 
-@client.event
-async def on_message(msg) -> None:
-    if msg.author.id == client.user.id: return
-
-    print( f"{Fore.CYAN}[{datetime.datetime.now(pytz.timezone('Europe/Berlin')).strftime('%H:%M:%S')}] {Fore.RESET}{msg.author.name}: {msg.content}" )
 
 client.run(cfg.data['dc']['token'])
